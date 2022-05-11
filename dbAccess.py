52b7a821485156a23c56a0bb9126fd8d9966e9aa
oscar mata <oscar.delarosa@iteso.mx>
2022-05-11 00:41:25 -0500

    Version 8.3

--- dbAccess.py
+++ dbAccess.py
@@ -3,43 +3,7 @@ from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.Table('GraphGeneratorTest3')
-bytes_size = table.table_size_bytes
-
-if bytes_size > 0:
-    print("delete table")
-    table.delete()
-else:
-    print("table does not exist")
-
-table = dynamodb.create_table (
-    TableName = 'GraphGeneratorTest3',
-       KeySchema = [
-           {
-               'AttributeName': 'reqId',
-               'KeyType': 'HASH'
-           },
-           {
-               'AttributeName': 'graph',
-               'KeyType': 'RANGE'
-           }
-           ],
-           AttributeDefinitions = [
-               {
-                   'AttributeName': 'reqId',
-                   'AttributeType': 'S'
-               },
-               {
-                   'AttributeName':'graph',
-                   'AttributeType': 'S'
-               }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits':1,
-                'WriteCapacityUnits':1
-            }
-          
-    )
+table = dynamodb.Table('GraphGeneratorTable')
 
 element_id = 0
 
